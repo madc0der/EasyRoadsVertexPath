7302ff707569d7a89de8d3eeff2b45d69ef7f172
madc0der <oleg.vashenkov@gmail.com>
2022-03-17 02:25:23 +0300

    Added custom editor for curb builder. Implemented handles for start/end points for curb

--- ERVertexPath/ERPathAdapter.cs
+++ ERVertexPath/ERPathAdapter.cs
@@ -29,6 +29,11 @@ namespace ERVertexPath
 
         public float[] Distances => distances;
 
+        public Dictionary<int, GameObject> GetStartIndexToRoadMap()
+        {
+            return startIndexToRoadMap;
+        }
+
         public void InitFromWrapper(ERPathToVertexPathWrapper wrapper)
         {
             InitFromData(wrapper.TotalDistance, wrapper.Positions, wrapper.Directions, wrapper.Normals, 
